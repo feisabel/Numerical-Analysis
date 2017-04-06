65e0c05a63d72d51dce797dd528d2d191ea8f6b2
feisabel <fernandampi@yahoo.com.br>
2017-04-05 23:31:53 -0400

    Add Gauss_Seidel

--- Gauss_Seidel.m
+++ Gauss_Seidel.m
@@ -1,4 +1,4 @@
-function x = Jacobi(A, b, prev, eps)
+function x = Gauss_Seidel(A, b, prev, eps)
 n = size(A,1);
 x = zeros(n, 1);
 nm = eps;
@@ -7,10 +7,11 @@ while nm >= eps
     steps = steps + 1;
     for i = 1:n
         sum = 0;
-        for j = 1:n
-            if i ~= j
-                sum = sum + A(i,j)*prev(j);
-            end
+        for j = 1:i-1
+               sum = sum + A(i,j)*x(j);
+        end
+        for j = i+1:n
+               sum = sum + A(i,j)*prev(j);
         end
         sum = sum - b(i);
         x(i) = -1 * sum / A(i,i);
